6998c2a406d678967db1cc470f8c658a1ee44dff
Pieter Van der Haegen <vdhpieter@outlook.com>
2018-12-05 17:30:21 +0000

    Solutions day 1

---
+++ solutions/day1_puzzle2.rb
@@ -0,0 +1,47 @@
+# --- Part Two ---
+# You notice that the device repeats the same frequency change list over and over.
+# To calibrate the device, you need to find the first frequency it reaches twice.
+#
+# For example, using the same list of changes above, the device would loop as follows:
+#
+# Current frequency  0, change of +1; resulting frequency  1.
+# Current frequency  1, change of -2; resulting frequency -1.
+# Current frequency -1, change of +3; resulting frequency  2.
+# Current frequency  2, change of +1; resulting frequency  3.
+# (At this point, the device continues from the start of the list.)
+# Current frequency  3, change of +1; resulting frequency  4.
+# Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+#
+# In this example, the first frequency reached twice is 2.
+# Note that your device might need to repeat its list of frequency changes many
+# times before a duplicate frequency is found, and that duplicates might be found
+# while in the middle of processing the list.
+#
+# Here are other examples:
+#
+# +1, -1 first reaches 0 twice.
+# +3, +3, +4, -2, -4 first reaches 10 twice.
+# -6, +3, +8, +5, -6 first reaches 5 twice.
+# +7, +7, -2, -7, -4 first reaches 14 twice.
+#
+# What is the first frequency your device reaches twice?
+require 'csv'
+
+frequency = 0
+past_frequencies = [0]
+found_it = false
+
+while(true)
+  CSV.foreach('input/day1.csv', headers: false) do |row|
+    row_array = row[0].partition(/\+|-/)
+    operator = row_array[1]
+    number = row_array[2].to_i
+    frequency = operator == '+' ? frequency + number : frequency - number
+    found_it = past_frequencies.include?(frequency)
+    break if found_it
+    past_frequencies.push(frequency)
+  end
+  break if found_it
+end
+
+p "The first frequency your device reaches twice is: #{frequency}"
